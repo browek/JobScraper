9a2a3020ab01083127c46cc1942731eb68586c2d
Marek Kotulski <marek.kotulski@febrit.co>
2022-09-21 19:16:30 +0200

    Initial commit

---
+++ apps/frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'job-scraper-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'frontend';
+}
