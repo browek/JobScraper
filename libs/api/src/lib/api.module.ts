e4841f0385ba4871f331ab473697e2ab96ddc7fb
Marek Kotulski <nefiras@o2.pl>
2022-09-27 03:31:44 +0200

    add mysql db

--- libs/api/src/lib/api.module.ts
+++ libs/api/src/lib/api.module.ts
@@ -1,10 +1,24 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ApiController } from './api/api.controller';
 import { ApiService } from './api/api.service';
+import { PuppeteerService } from './puppeteer/puppeteer.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'root',
+      database: 'job_scraper',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
   controllers: [ApiController],
-  providers: [ApiService],
+  providers: [ApiService, PuppeteerService],
   exports: [],
 })
 export class ApiModule {}
