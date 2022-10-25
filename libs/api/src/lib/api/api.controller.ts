df05bda69711106a6d8b4280681db9475e4642d5
Marek Kotulski <nefiras@o2.pl>
2022-10-25 23:36:41 +0200

    add puppeteer service

--- libs/api/src/lib/api/api.controller.ts
+++ libs/api/src/lib/api/api.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { PuppeteerService } from '../puppeteer/puppeteer.service';
 import { ApiService } from './api.service';
 
 @Controller('api')
 export class ApiController {
-    constructor(private apiService: ApiService) { }
+    constructor(
+        private apiService: ApiService,
+        private puppeteerService: PuppeteerService,
+
+        ) { }
 
     @Get('records')
-    getUsers() {
+    async getUsers() {
+        this.puppeteerService.getData()
+
         return this.apiService.getRecords()
     }
 }
