16cd89a89efbe6a80e4a8679c2ce6d23d6492da9
Marek Kotulski <nefiras@o2.pl>
2022-11-12 00:40:07 +0100

    change window size

--- libs/api/src/lib/api/services/scrapper/scrapper.service.ts
+++ libs/api/src/lib/api/services/scrapper/scrapper.service.ts
@@ -5,48 +5,80 @@ import { OfferService } from '../offer/offer.service';
 @Injectable()
 export class ScrapperService {
     constructor(private offerService: OfferService) {}
+
     async getData() {
 
-        const browser = await puppeteer.launch(
-            { headless: false }
+        const browser = await puppeteer.launch({ 
+            headless: false,
+            devtools: true,
+            slowMo: 1000,
+            }
             );
         const page = await browser.newPage();
         await page.goto('https://justjoin.it/remote/javascript/junior');
         await page.setViewport({
-            width: 1200,
-            height: 800
+            width: 1000,
+            height: 1800
         });
 
-        const subLink = await page.evaluate(() => {
-            const table = document.querySelector('#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div > div')
-            const offer = table.querySelector('div:nth-child(1)') 
-            const offerLink: string = offer.querySelector('a').href.trim() //link
-
-           return offerLink
+        const allOffers = await page.evaluate(() => {
+            const allOffers = document.querySelector("#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-13jvhmn > div.css-1x7tcfa > a.css-agbnxz > div > div > span").textContent.trim()
+            return parseInt(allOffers)
         });
 
-        this.offerService.findOneOffer(subLink)
-        .then(() => console.log('Existed link'))
-        .catch(() => {
-            this.newTab(browser, subLink)
-        })
-        
-        
-        // // scroll
-        // const scrollable_section = '#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div'
-        // await page.evaluate(selector => {
-        //     const scrollableSection = document.querySelector(selector);
-        //     scrollableSection.scrollTop = scrollableSection.scrollTop + 1000;
-        //   }, scrollable_section);
+        let offerNumber = 0
+        do {
+            offerNumber ++
+            console.log('get link')
+            const subLink = await page.evaluate((offerN) => {
+                console.log(offerN)
+                const table = document.querySelector('#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div > div')
+
+                // const offer = table.querySelector(`div:nth-child(${offerN})`) 
+                const offerLink = table.querySelectorAll('a')[offerN].href.trim()
+
+                // const offerLink: string = offer.querySelector('a').href.trim()
+                const visibleOffers = (table.childElementCount)
+
+                return {offerLink, visibleOffers}
+            },offerNumber);
+            
+            console.log(subLink.offerLink)
+            this.offerService.findOneOffer(subLink.offerLink)
+            .then(() => console.log('Existed link'))
+            .catch(async () => {
+                await this.newTab(browser, subLink.offerLink)
+            })
+            // console.log(subLink.visibleOffers)
+            
+            if(offerNumber>subLink.visibleOffers) {
+                this.scroll(page)
+            }
+            console.log(offerNumber, subLink.visibleOffers,allOffers)
+        } while(offerNumber < allOffers)
+    }
+
+    async scroll(page) {
+        const scrollable_section = '#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div'
+        await page.evaluate(selector => {
+            const scrollableSection = document.querySelector(selector);
+            scrollableSection.scrollTop = scrollableSection.scrollTop + 1000;
+          }, scrollable_section);
         // await browser.close();
         // await page.evaluate(() => {
         //     const table = document.querySelector('#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div > div')
         //     console.log(table.querySelector('div:nth-child(1)'))
       
         // });
+
     }
+    
     async newTab(browser, subLink) {
         const newOfferTab = await browser.newPage();
+        await newOfferTab.setViewport({
+            width: 1000,
+            height: 1800
+        });
         await newOfferTab.goto(subLink);
 
         const offerDetails = await newOfferTab.evaluate(() => {
@@ -56,10 +88,10 @@ export class ScrapperService {
                 const salary = mainTab.querySelector("div.css-1wla3xl").textContent.trim()
             // info
             const infoTab = document.querySelector('#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-1pc9k2p > div.css-vuh3mm > div.css-1kgdb8a')
-                const offerCompany = infoTab.querySelector('div:nth-child(1) > a').textContent.trim()
+                const company = infoTab.querySelector('div:nth-child(1) > a').textContent.trim()
                 const expLvl = infoTab.querySelector('div:nth-child(3) > div.css-1ji7bvd').textContent.trim()
             // tech stack
-            const techStackTab = document.querySelectorAll("#root > div > div.css-1xh23hj > div > div.css-1pc9k2p > div:nth-child(2) > div:nth-child(2) > div.css-1xc9aks > div > div")
+            const techStackTab = document.querySelectorAll("#root > div > div.css-1xh23hj > div > div.css-1pc9k2p > div:nth-child(2) > div:nth-child(2) > div.css-1xc9aks > div")
             const techStack = {};
             // const techStack = [];
             techStackTab.forEach(elem => {
@@ -70,7 +102,7 @@ export class ScrapperService {
             });
             const offerDetails = {
                 name,
-                offerCompany,
+                company,
                 salary,
                 expLvl,
                 techStack
@@ -81,7 +113,7 @@ export class ScrapperService {
         offerDetails.link = subLink
         console.log(offerDetails)
 
-            // return offerDetails
+        this.createOffer(offerDetails)
     }
     
     createOffer(offerDetails) {
