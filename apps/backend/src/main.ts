eff754844a7282ab8ce0f20537703176ce858985
Marek Kotulski <nefiras@o2.pl>
2022-11-12 21:48:49 +0100

    api: add enable cors

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -12,8 +12,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = '';
   app.setGlobalPrefix(globalPrefix);
+  app.enableCors();
   const port = process.env.PORT || 3333;
   await app.listen(port);
+  
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
