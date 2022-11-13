2df84a2889b664844cef5f3dd0faeb440956d11c
Marek Kotulski <nefiras@o2.pl>
2022-11-13 01:54:06 +0100

    add offers service

---
+++ libs/ui/src/lib/services/offers.service.ts
@@ -0,0 +1,15 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OffersService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllOffers() {
+    return this.httpClient.get('http://localhost:3333/api/records')
+  }
+}
