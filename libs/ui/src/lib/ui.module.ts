ab7c3b5fe1d66fb12515e06a566a45239a636114
Marek Kotulski <nefiras@o2.pl>
2022-11-13 01:55:21 +0100

    to offers service

--- libs/ui/src/lib/ui.module.ts
+++ libs/ui/src/lib/ui.module.ts
@@ -2,9 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { UiRoutingModule } from './ui-routing.module';
+import { JustjoinitComponent } from './feature-modules/justjoinit/justjoinit/component/justjoinit.component';
+import { HttpClientModule } from '@angular/common/http'
+import { OffersService } from './services/offers.service';
+
+
+
 
 @NgModule({
-  imports: [CommonModule, UiRoutingModule],
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, JustjoinitComponent],
+  imports: [
+    CommonModule,
+    UiRoutingModule,
+    HttpClientModule
+  ],
+  providers: [
+    OffersService
+  ]
 })
 export class UiModule {}
