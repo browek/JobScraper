2ea66ce439bd9cdfcad65d961ff99d40e3d50a0c
Marek Kotulski <nefiras@o2.pl>
2022-11-15 22:00:09 +0100

    api: add scrap img

--- libs/api/src/lib/api/services/scrapper/scrapper.service.ts
+++ libs/api/src/lib/api/services/scrapper/scrapper.service.ts
@@ -6,40 +6,48 @@ import { OfferService } from '../offer/offer.service';
 export class ScrapperService {
     constructor(private offerService: OfferService) {}
 
-    async getData() {
+    getData() {
+        const linksTab = [
+            'https://justjoin.it/remote/javascript/junior',
+            'https://justjoin.it/remote/testing/junior'
+    ]
+    linksTab.forEach(link => {
+        this.scrap(link)
+    });
+    }
 
+    async scrap(link) {
         const browser = await puppeteer.launch({ 
             headless: false,
             devtools: true,
             slowMo: 1000,
-            }
-            );
+        });
         const page = await browser.newPage();
-        await page.goto('https://justjoin.it/remote/javascript/junior');
+        await page.goto(link, {timeout: 0});
         await page.setViewport({
             width: 1000,
             height: 1800
         });
-
+    
         const allOffers = await page.evaluate(() => {
             const allOffers = document.querySelector("#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-13jvhmn > div.css-1x7tcfa > a.css-agbnxz > div > div > span").textContent.trim()
             return parseInt(allOffers)
         });
-
-        let offerNumber = 0
+    
+        let offerNumber = 0;
         do {
-            offerNumber ++
             console.log('get link')
             const subLink = await page.evaluate((offerN) => {
                 console.log(offerN)
                 const table = document.querySelector('#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div > div')
-
+                
                 // const offer = table.querySelector(`div:nth-child(${offerN})`) 
                 const offerLink = table.querySelectorAll('a')[offerN].href.trim()
-
+                
                 // const offerLink: string = offer.querySelector('a').href.trim()
                 const visibleOffers = (table.childElementCount)
-
+                
+                
                 return {offerLink, visibleOffers}
             },offerNumber);
             
@@ -55,12 +63,14 @@ export class ScrapperService {
                 this.scroll(page)
             }
             console.log(offerNumber, subLink.visibleOffers,allOffers)
+            offerNumber ++
         } while(offerNumber < allOffers)
         page.close()
             setTimeout(() => {  
                     browser.close()
             }, 60000)
     }
+
 
     async scroll(page) {
         const scrollable_section = '#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-110u7ph > div:nth-child(1) > div'
@@ -81,11 +91,12 @@ export class ScrapperService {
             width: 1000,
             height: 1800
         });
-        await newOfferTab.goto(subLink);
+        await newOfferTab.goto(subLink, {timeout: 0});
 
         const offerDetails = await newOfferTab.evaluate(() => {
             // main
-            const mainTab = document.querySelector("#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-1pc9k2p > div.css-vuh3mm > div.css-1jqmyqn > div")
+            const mainTab = document.querySelector("#root > div.css-1smbjja > div.css-1xh23hj > div > div.css-1pc9k2p > div.css-vuh3mm")
+                const img = mainTab.querySelector('img').src.trim()
                 const name = mainTab.querySelector('div.css-1id4k1').textContent.trim()
                 const salary = mainTab.querySelector("div.css-1wla3xl").textContent.trim()
             // info
@@ -109,7 +120,8 @@ export class ScrapperService {
                 company,
                 salary,
                 expLvl,
-                techStack
+                techStack,
+                img
             }
             console.log(offerDetails)
             return offerDetails
